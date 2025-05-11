2c6aa3aa87bcb5090d53e72876ad32740292f2af
deageorge283 <deageorge283@gmail.com>
2025-05-11 18:20:28 +0530

    Update

--- module_sample/models/sales.py
+++ module_sample/models/sales.py
@@ -7,3 +7,9 @@ class SaleOrder(models.Model):
 	
 	model_one_id = fields.Many2one('model.one', string="Model One")
 	description = fields.Text("Description", default="Test Description")
+	
+	@api.depends('partner_id')
+	def _compute_partner_invoice_id(self):
+		for order in self:
+			order.description = "Added by a method"
+		return super(SaleOrder, self)._compute_partner_invoice_id()
